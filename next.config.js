a2fc25dae4c55dc3f9718543da3919e5b846d5c9
Evan Walter <evan.walter.software@gmail.com>
2022-08-27 20:48:07 -0700

    more config

--- next.config.js
+++ next.config.js
@@ -5,12 +5,4 @@ const nextConfig = {
 
 module.exports = {
   trailingSlash: false,
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-
-    return config
-  },
 }
