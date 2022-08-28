d7d9a07c42a36b0e190e1ceb1b01a7779baf327b
Evan Walter <evan.walter.software@gmail.com>
2022-08-27 20:25:24 -0700

    config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  trailingSlash: false,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+
+    return config
+  },
+}
